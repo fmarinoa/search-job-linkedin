1de20ec032f83cdba2573df2cdd9d3afff1d6708
FRAMAR <franco.edson.18014@gmail.com>
2025-03-23 17:14:54 -0500

    add: Obtengo los datos del reclutador, su perfil y descripción extendida de cada trabajo

--- app/utils/SearchJob.py
+++ app/utils/SearchJob.py
@@ -30,7 +30,8 @@ def scrape_jobs(job_description: str, location: str) -> []:
         xpath_base = "//*[@id='main-content']/section[2]/ul/li"
 
         results = tree.xpath(xpath_base)  # Obtiene todos los div con esa clase
-        data.append(["Title job", "Employer", "Link profile employer", "Location", "How long ago", "Link offer"])
+        data.append(["Title job", "Employer", "Link profile employer", "Location", "How long ago", "Recruiter",
+                     "Profile recruiter", "Description offer", "Link offer"])
 
         for i in range(1, len(results) + 1):
             xpath_item = f"({xpath_base})[{i}]/*"
@@ -40,9 +41,42 @@ def scrape_jobs(job_description: str, location: str) -> []:
             location = tree.xpath(xpath_item + "/div[2]/div/span")[0].text.strip().replace('"', '')
             how_long_ago = tree.xpath(xpath_item + "/div[2]/div/time")[0].text.strip()
             link_job = tree.xpath(xpath_item + "/a/@href")[0]
-            # print(f"Title: {title_job}, Employer: {employer}, Profile employer: {profile_employer}, Location: {location}, Link: {link_job}, Published ago: {how_long_ago}")
-            data.append([title_job, employer, profile_employer, location, how_long_ago, link_job])
+            description_offer = get_description_offer(link_job)
+            data.append([title_job, employer, profile_employer, location, how_long_ago, description_offer[0],
+                         description_offer[1], description_offer[2], link_job])
     except Exception as e:
-        return data.append(f"Ocurrió un erro inesperado: {e}")
+        logger.error(f"Ocurrió un error inesperado: {e}")
+        return
 
     return data
+
+
+def get_description_offer(link: str) -> [str]:
+    """ Obtiene la información de una oferta de trabajo.
+
+    Retorna una lista con:
+    1. Nombre del reclutador (o vacío si no existe)
+    2. Link del perfil del reclutador (o vacío si no existe)
+    3. Descripción del trabajo (o vacío si no existe)
+    """
+    try:
+        tree = html.fromstring(requests.get(link).content)
+
+        # XPath de los 3 datos a extraer
+        xpath_recruiter_name = "//*[@id='main-content']/section[1]/div/div/section[1]/div/div[1]/div/a/span/text()"
+        xpath_recruiter_link = "//*[@id='main-content']/section[1]/div/div/section[1]/div/div[1]/div/a/@href"
+        xpath_job_description = "//*[@id='main-content']/section[1]/div/div/section[1]/div/div[last()]/section/div/text()"
+
+        # Extraer datos o devolver ""
+        recruiter_name = tree.xpath(xpath_recruiter_name)
+        recruiter_link = tree.xpath(xpath_recruiter_link)
+        job_description = tree.xpath(xpath_job_description)
+
+        return [
+            recruiter_name[0].strip() if recruiter_name else "",
+            recruiter_link[0].strip() if recruiter_link else "",
+            job_description[0].strip() if job_description else ""
+        ]
+    except Exception as e:
+        logger.error(f"Error al obtener información de la oferta: {e}")
+        return ["", "", ""]  # Si hay error, devuelve 3 valores vacíos
