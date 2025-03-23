7bfb76f5364b551f8f13fc11674a2c3136b355a6
Franco Mariño <123432025+fmarinoa@users.noreply.github.com>
2025-03-23 00:57:03 -0500

    Update Constants.py

--- app/utils/Constants.py
+++ app/utils/Constants.py
@@ -1,5 +1,5 @@
 class Constants:
     TIME_OUT = 60
     URL_BASE = "https://www.linkedin.com/login"
-    CHROME_DRIVER_PATH = "resources\\drivers\\chromedriver.exe"
-    EDGE_DRIVER_PATH = "resources\\drivers\\msedgedriver.exe"
+    CHROME_DRIVER_PATH = "resources/drivers/chromedriver.exe"
+    EDGE_DRIVER_PATH = "resources/drivers/msedgedriver.exe"
