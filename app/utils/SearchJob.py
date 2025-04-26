4c65076231529eb730e4213449f122003d418b0c
FRAMAR <franco.edson.18014@gmail.com>
2025-04-26 12:14:47 -0500

    Se arma html con json local

--- app/utils/SearchJob.py
+++ app/utils/SearchJob.py
@@ -1,7 +1,8 @@
-import requests
-from lxml import html
+from urllib.parse import urlencode
 
+import requests
 from app.utils.logger_config import get_logger
+from lxml import html
 
 logger = get_logger(__name__)
 
@@ -13,10 +14,15 @@ def scrape_jobs(job_description: str, location: str) -> []:
     data = []
 
     try:
-        job_description = job_description.replace(" ", "%20")
-        location = location.replace(" ", "%20")
+        params = {
+            "keywords": job_description,
+            "location": location,
+            "f_TPR": "r604800"
+        }
 
-        url = f"https://www.linkedin.com/jobs/search?keywords={job_description}&location={location}&f_TPR=r604800"
+        base_url = "https://www.linkedin.com/jobs/search"
+        encoded_params = urlencode(params)
+        url = f"{base_url}?{encoded_params}"
 
         response = requests.get(url)
 
@@ -34,16 +40,20 @@ def scrape_jobs(job_description: str, location: str) -> []:
                      "Profile recruiter", "Description offer", "Link offer"])
 
         for i in range(1, len(results) + 1):
-            xpath_item = f"({xpath_base})[{i}]/*"
-            title_job = tree.xpath(xpath_item + "/div[2]/h3")[0].text.strip()
-            employer = tree.xpath(xpath_item + "/div[2]/h4/a")[0].text.strip()
-            profile_employer = tree.xpath(xpath_item + "/div[2]/h4/a/@href")[0]
-            location = tree.xpath(xpath_item + "/div[2]/div/span")[0].text.strip().replace('"', '')
-            how_long_ago = tree.xpath(xpath_item + "/div[2]/div/time")[0].text.strip()
-            link_job = tree.xpath(xpath_item + "/a/@href")[0]
-            description_offer = get_description_offer(link_job)
-            data.append([title_job, employer, profile_employer, location, how_long_ago, description_offer[0],
-                         description_offer[1], description_offer[2], link_job])
+            try:
+                xpath_item = f"({xpath_base})[{i}]/*"
+                title_job = tree.xpath(xpath_item + "/div[2]/h3")[0].text.strip()
+                employer = tree.xpath(xpath_item + "/div[2]/h4/a")[0].text.strip()
+                profile_employer = tree.xpath(xpath_item + "/div[2]/h4/a/@href")[0]
+                location = tree.xpath(xpath_item + "/div[2]/div/span")[0].text.strip().replace('"', '')
+                how_long_ago = tree.xpath(xpath_item + "/div[2]/div/time")[0].text.strip()
+                link_job = tree.xpath(xpath_item + "/a/@href")[0]
+                description_offer = get_description_offer(link_job)
+                data.append([title_job, employer, profile_employer, location, how_long_ago, description_offer[0],
+                             description_offer[1], description_offer[2], link_job])
+            except Exception as e:
+                logger.error(f"Ocurrió un error inesperado al capturar los datos para {title_job}: {e}")
+                continue
     except Exception as e:
         logger.error(f"Ocurrió un error inesperado: {e}")
         return
@@ -73,7 +83,12 @@ def get_description_offer(link: str) -> [str]:
         job_description_node = tree.xpath(xpath_job_description)
 
         # Obtener todo el contenido del nodo (incluyendo texto dentro de etiquetas <br>, <strong>, etc.)
-        job_description = job_description_node[0].xpath("string(.)").strip().encode('latin1').decode('utf-8') if job_description_node else ""
+        job_description = (job_description_node[0]
+                           .xpath("string(.)")
+                           .strip()
+                           .encode('latin1')
+                           .decode('utf-8')
+                           ) if job_description_node else ""
 
         return [
             recruiter_name[0].strip() if recruiter_name else "",
--- generate_html.py
+++ generate_html.py
@@ -1,57 +1,53 @@
-from pathlib import Path
-
-import requests
+import json
 
 from app.utils.logger_config import get_logger
 
 logger = get_logger(__name__)
 
-project_path = Path().absolute()
-results_path = str(project_path) + "/results/"
+results_path = "./results/"
 html_path = f"{results_path}email_body.html"
+json_path = f"{results_path}offers.json"
 
-url = "https://raw.githubusercontent.com/fmarinoa/search-job-linkedin/main/results/offers.json"
+with open(json_path, "r", encoding="utf-8") as file:
+    offers = json.load(file)
 
-response = requests.get(url)
-offers = response.json()
-
-html_content = """<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style>
-            body { font-family: Arial, sans-serif; }
-            .offer { margin-bottom: 20px; border-bottom: 1px solid #ccc; padding-bottom: 10px; }
-            .title { font-size: 18px; font-weight: bold; color: #333; }
-            .company { color: #0073b1; }
-            .link { text-decoration: none; color: #0073b1; }
-        </style>
-    </head>
-    <body>
-        <h2>Últimas Ofertas de Trabajo</h2>
-        <p>Datos extraídos de https://github.com/fmarinoa/search-job-linkedin/tree/main/results</p>
-    """
-
-for offer in offers:
-    html_content += f"""
-        <div class="offer">
-            <p class="title">{offer['Title job']}</p>
-            <p><a class="link" href="{offer['Link profile employer']}">{offer['Employer']}</a></p>"""
-
-    html_content += f"""<p><a class="link" href="{offer['Profile recruiter']}">{offer['Recruiter']}</a></p>""" if \
-        offer['Recruiter'] else ""
-
-    html_content += f"""
-            <p>📍 {offer['Location']} | ⏳ {offer['How long ago']}</p>
-            <p>{offer['Description offer'][:300]}...</p>
-            <p><a class="link" href="{offer['Link offer']}">Ver oferta completa</a></p>
-        </div>
+    html_content = """<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <style>
+                body { font-family: Arial, sans-serif; }
+                .offer { margin-bottom: 20px; border-bottom: 1px solid #ccc; padding-bottom: 10px; }
+                .title { font-size: 18px; font-weight: bold; color: #333; }
+                .company { color: #0073b1; }
+                .link { text-decoration: none; color: #0073b1; }
+            </style>
+        </head>
+        <body>
+            <h2>Últimas Ofertas de Trabajo</h2>
+            <p>Datos extraídos de https://github.com/fmarinoa/search-job-linkedin/tree/main/results</p>
         """
 
-html_content += "</body></html>"
+    for offer in offers:
+        html_content += f"""
+            <div class="offer">
+                <p class="title">{offer['Title job']}</p>
+                <p><a class="link" href="{offer['Link profile employer']}">{offer['Employer']}</a></p>"""
 
-with open(html_path, "w", encoding="utf-8") as f:
-    f.write(html_content)
+        html_content += f"""<p><a class="link" href="{offer['Profile recruiter']}">{offer['Recruiter']}</a></p>""" if \
+            offer['Recruiter'] else ""
 
-logger.info(f"✅ html guardado en {html_path}'.")
+        html_content += f"""
+                <p>📍 {offer['Location']} | ⏳ {offer['How long ago']}</p>
+                <p>{offer['Description offer'][:300]}...</p>
+                <p><a class="link" href="{offer['Link offer']}">Ver oferta completa</a></p>
+            </div>
+            """
+
+    html_content += "</body></html>"
+
+    with open(html_path, "w", encoding="utf-8") as f:
+        f.write(html_content)
+
+    logger.info(f"✅ html guardado en {html_path}'.")
