dd5ff8a22b23dc143eda45d160f3a63cd52db433
fmarinoa <franco.edson.18014@gmail.com>
2025-06-07 17:06:34 -0500

    Optimizar la obtención del archivo CSV más reciente y ajustar la estructura de carpetas para el almacenamiento de resultados

--- app/utils/Util.py
+++ app/utils/Util.py
@@ -9,7 +9,7 @@ from app.utils.logger_config import get_logger
 project_path = Path().absolute()
 today = datetime.now().strftime("%Y%m%d")
 
-results_path = str(project_path) + "/results/"
+results_path = str(project_path) + "\\results\\"
 today_path = results_path + str(today)
 
 logger = get_logger(__name__)
@@ -23,18 +23,29 @@ def create_folder() -> None:
         logger.info(f"📁 La carpeta '{today_path}' ya existe.")
 
 
-def append_results_csv(results: []) -> None:
+def get_day_folder_path() -> str:
+    """Devuelve la ruta results/año/mes/día y crea la carpeta si no existe."""
+    now = datetime.now()
+    year = now.strftime("%Y")
+    month = now.strftime("%m")
+    day = now.strftime("%d")
+    day_path = os.path.join(results_path, year, month, day)
+    os.makedirs(day_path, exist_ok=True)
+    return day_path
+
+def save_results_csv(results: [], folder_path: str) -> None:
     timestamp = datetime.now().strftime("%H%M%S")
-    csv_path = f"{today_path}/offers_{timestamp}.csv"
-
-    # Extraer encabezados de la primera fila y asignarlos a cada elemento
-    headers = results[0]  # Primera fila como nombres de columnas
-    data = results[1:]  # Datos reales
-
-    # Crear DataFrame con encabezados correctos
+    csv_path = os.path.join(folder_path, f"offers_{timestamp}.csv")
+    headers = results[0]
+    data = results[1:]
     df = pd.DataFrame(data, columns=headers)
-    df.to_csv(csv_path, index=False, index_label=False, encoding="utf-8")
-    logger.info(f"✅ CSV guardado en {csv_path}'.")
+    df.to_csv(csv_path, index=False, encoding="utf-8")
+    logger.info(f"✅ CSV guardado en {csv_path}.")
+
+def append_results_csv(results: []) -> None:
+    """Función principal para guardar resultados en CSV en la estructura año/mes/día."""
+    folder_path = get_day_folder_path()
+    save_results_csv(results, folder_path)
 
 
 def append_results_json(results: []) -> None:
