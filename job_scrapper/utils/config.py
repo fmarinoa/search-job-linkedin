224c9e8d7da31092c162fe5a73ec037e292b3833
fmarinoa <franco.edson.18014@gmail.com>
2025-09-19 21:24:40 -0500

    Elimina el archivo api.py

--- job_scrapper/utils/config.py
+++ job_scrapper/utils/config.py
@@ -4,6 +4,4 @@ from job_scrapper.utils.constants import SETTINGS_YAML
 
 def load_config() -> dict:
     with open(Path(SETTINGS_YAML), "r", encoding="utf-8") as f:
-        config = yaml.safe_load(f)
-        print(config) 
-        return config
+        return yaml.safe_load(f)
